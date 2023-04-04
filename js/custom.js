3dea5b115db5d851af6138c1e0798d1a139319df
DESKTOP-1IUK8D9\javie <javier.rlc@hotmail.com>
2023-04-04 14:03:34 +0200

    changes

--- js/custom.js
+++ js/custom.js
@@ -8,17 +8,7 @@
   });
 
   // Scroll
-  $(function() {
 
-    var $body = $(document);
-    $body.bind('scroll', function() {
-        // "Desactivar" el scroll horizontal
-        if ($body.scrollLeft() !== 0) {
-            $body.scrollLeft(0);
-        }
-    });
-
-}); 
 
 
   // MENU
